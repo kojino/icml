4a35ddc17431eda39db1768db6feed9d9216c35a
juan carlos perdomo <jcperdomosilva@gmail.com>
2018-01-31 12:50:02 -0500

    finished adding ensemble comparisons, will run them on the cluster now

---
+++ ensemble_targeted.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+#SBATCH -t 3-00:00
+#SBATCH -p holyseasgpu
+#SBATCH --gres=gpu:1
+#SBATCH --mem=10000
+#SBATCH --constraint=cuda-7.5
+#SBATCH --mail-type=all
+#SBATCH --mail-user=jperdomo@college.harvard.edu
+
+NOISE_TYPE="targeted"
+
+CMD="python -m imagenet_ensemble -noise_type $NOISE_TYPE"
+eval $CMD
---
+++ mnist_targeted.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+#SBATCH -t 7-00:00
+#SBATCH -p holyseasgpu
+#SBATCH --gres=gpu:1
+#SBATCH --mem=10000
+#SBATCH --constraint=cuda-7.5
+#SBATCH --mail-type=all
+#SBATCH --mail-user=jperdomo@college.harvard.edu
+
+NOISE_TYPE="targeted"
+ALPHA=2.2
+DATA_SET="mnist"
+MWU_ITERS=95
+OPT_ITERS=3000
+LR=.001
+
+CMD="python -m deep_learning_experiments -data_set $DATA_SET -mwu_iters $MWU_ITERS -learning_rate $LR -noise_type $NOISE_TYPE -alpha $ALPHA -opt_iters $OPT_ITERS"
+eval $CMD
