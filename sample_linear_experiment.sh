7dfe5f9e485286f93730175ae5cbf8064f4041f9
Kojin Oshiba <kojin.oshiba@gmail.com>
2018-03-19 10:26:53 -0400

    add sample linear experiment helper

--- sample_linear_experiment.sh
+++ sample_linear_experiment.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+#SBATCH -t 1-0:00
+#SBATCH -p shared
+#SBATCH --mem=1000
+#SBATCH -o sample_linear_experiment.out
+#SBATCH -e sample_linear_experiment.err
+#SBATCH --mail-type=all
+#SBATCH --mail-user=kojinoshiba@college.harvard.edu
 
 ALPHA=.5
 EXP_TYPE="binary"
@@ -12,14 +19,14 @@ LIST_NUM_SAMPLES=$(seq 100)
 # Full set of classifiers
 SAMPLE="none"
 CMD="python -m linear_experiments -data_path $DATA_PATH -noise_func $NOISE_FUNC -iters $ITERS -num_classifiers $NUM_CLASSIFIERS -exp_type $EXP_TYPE -alpha $ALPHA -sample $SAMPLE"
-eval $CMD
+./sample_linear_experiment_helper.sh $CMD
 
 # Sample classifiers once
 SAMPLE="once"
 for NUM_SAMPLES in $LIST_NUM_SAMPLES
 do
   CMD="python -m linear_experiments -data_path $DATA_PATH -noise_func $NOISE_FUNC -iters $ITERS -num_classifiers $NUM_CLASSIFIERS -exp_type $EXP_TYPE -alpha $ALPHA -sample $SAMPLE -num_samples $NUM_SAMPLES"
-  eval $CMD
+  ./sample_linear_experiment_helper.sh $CMD
 done
 
 # Sample classifiers every iteration
@@ -27,5 +34,5 @@ SAMPLE="iter"
 for NUM_SAMPLES in $LIST_NUM_SAMPLES
 do
   CMD="python -m linear_experiments -data_path $DATA_PATH -noise_func $NOISE_FUNC -iters $ITERS -num_classifiers $NUM_CLASSIFIERS -exp_type $EXP_TYPE -alpha $ALPHA -sample $SAMPLE -num_samples $NUM_SAMPLES"
-  eval $CMD
+  ./sample_linear_experiment_helper.sh $CMD
 done
---
+++ sample_linear_experiment_helper.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH -t 1-0:00
+#SBATCH -p shared
+#SBATCH --mem=1000
+#SBATCH -o run_sample_linear_experiment_%j.out
+#SBATCH -e run_sample_linear_experiment_%j.err
+#SBATCH --mail-type=all
+#SBATCH --mail-user=kojinoshiba@college.harvard.edu
+
+eval $1
