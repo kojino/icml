0bc9bf9c293dd48e9a16aa48615aa3733c71e05c
Kojin Oshiba <kojin.oshiba@gmail.com>
2018-03-18 21:34:49 -0400

    add TODOs

--- setup_binary.py
+++ setup_binary.py
@@ -10,7 +10,7 @@ num_points = 100
 folder = "binary_data_2"
 
 data = MNIST()
-label_dict = dict(zip(classes, [-1, 1]))
+label_dict = dict(list(zip(classes, [-1, 1])))
 
 if not os.path.exists(folder):
     os.mkdir(folder)
@@ -27,23 +27,23 @@ train_size = len(X_train) / num_classifiers
 models = []
 training_sets = []
 accs = []
-for i in xrange(num_classifiers):
+for i in range(num_classifiers):
     start = train_size * i
     end = start + train_size
     training_sets.append([start, end])
     model = trainLBC(X_train[start:end], Y_train[start:end])
     acc = model.evaluate(X_test, Y_test)
-    print("Model {}, Start {}, End {}, Test Accuracy {}".format(i, start, end, acc))
+    print(("Model {}, Start {}, End {}, Test Accuracy {}".format(i, start, end, acc)))
     np.save(folder + "/" + "weights_{}.npy".format(i), model.weights)
     np.save(folder + "/" + "bias_{}.npy".format(i), model.bias)
     models.append(model)
     accs.append(acc)
 
 X_exp, Y_exp = helper.generate_data(num_points, X_test, Y_test, models)
-print "Confirmation that selected points are all correctly classified ", [model.evaluate(X_exp, Y_exp)
-                                                                          for model in models]
+print("Confirmation that selected points are all correctly classified ", [model.evaluate(X_exp, Y_exp)
+                                                                          for model in models])
 min_dists, max_dists = helper.findNoiseBoundsBinary(models, X_exp, Y_exp)
-print "Median Min Max Noise Bounds {} {}".format(np.median(min_dists), np.median(max_dists))
+print("Median Min Max Noise Bounds {} {}".format(np.median(min_dists), np.median(max_dists)))
 
 np.save(folder + "/" + "test_accuracies.npy", np.array(accs))
 np.save(folder + "/" + "dists.npy", np.array([min_dists, max_dists]))
